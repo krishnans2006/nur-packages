3c7012573f01037656bb7fe727a1f07047e8cd09
Krishnan Shankar <krishnans2006@gmail.com>
2026-06-29 00:25:52 -0400

    Add chipwhisperer
    See https://github.com/krishnans2006/chipwhisperer-flake.

--- default.nix
+++ default.nix
@@ -19,6 +19,8 @@
   # flakeModules = { }; # flake-parts modules
   # overlays = import ./overlays; # nixpkgs overlays
 
+  chipwhisperer = pkgs.callPackage ./pkgs/chipwhisperer { };
+
   # example-package = pkgs.callPackage ./pkgs/example-package { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
